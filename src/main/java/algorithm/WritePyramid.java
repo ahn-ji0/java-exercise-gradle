4a7712ec2feb46ce2d23c1cf94cb6623ecd2bc8a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-17 09:49:10 +0900

    피라미드 별찍기

---
+++ src/main/java/algorithm/WritePyramid.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+public class WritePyramid {
+    public void printPyramid(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j < n-i; j++)
+                System.out.print(" ");
+            for(int k =0 ; k < 2*i -1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        //Scanner sc = new Scanner(System.in);
+        WritePyramid writeTriangle = new WritePyramid();
+        writeTriangle.printPyramid(4);
+    }
+}
