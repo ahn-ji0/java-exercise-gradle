203882cd642f65fda4f838f478e231fd97cf645f
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-17 09:18:58 +0900

    별찍기

--- src/main/java/algorithm/WriteTriangle.java
+++ src/main/java/algorithm/WriteTriangle.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class WriteTriangle {
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in);
-        for(int i=1;i<=4;i++){
-            for(int j =0 ;j<i;j++) {
+        for(int i=1; i<=4; i++){
+            for(int j =0 ;j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
