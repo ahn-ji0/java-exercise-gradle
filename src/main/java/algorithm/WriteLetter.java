5111be2388bee274b3a203a3d3c7b31d0e3c0402
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-17 09:37:45 +0900

    별찍기 - 어떤 letter 출력할지 선택 가능

--- src/main/java/algorithm/WriteLetter.java
+++ src/main/java/algorithm/WriteLetter.java
@@ -12,16 +12,19 @@ public class WriteLetter {
     public char getLetter() {
         return letter;
     }
+    public void printTriangle(int n){
+        for(int i=1; i<=n; i++){
+            for(int j =0 ;j < i; j++) {
+                System.out.print(this.letter);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char letter = sc.next().charAt(0);
         WriteLetter writeLetter = new WriteLetter(letter);
-        for(int i=1; i<=4; i++){
-            for(int j =0 ;j < i; j++) {
-                System.out.print(writeLetter.getLetter());
-            }
-            System.out.println();
-        }
+        writeLetter.printTriangle(4);
     }
 }
