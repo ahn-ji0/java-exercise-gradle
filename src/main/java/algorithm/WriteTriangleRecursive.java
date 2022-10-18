8ee927bee68b70efbb66732191586ddd0424ca29
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-18 09:46:38 +0900

    별찍기 재귀(복합)

--- src/main/java/algorithm/WriteTriangleRecursive.java
+++ src/main/java/algorithm/WriteTriangleRecursive.java
@@ -2,17 +2,26 @@ package algorithm;
 
 //별찍기 직각삼각형
 public class WriteTriangleRecursive {
-    public void printTriangle(int n){
+    public void printRow(int n){
         if(n==1){
-            System.out.println("*");
+            System.out.print("*");
+            return;
+        }
+        printRow(n-1);
+        System.out.print("*");
+    }
+
+    public void printTriangle(int n){
+        if(n==0){
             return;
         }
         printTriangle(n-1);
-        System.out.println("*".repeat(n));
+        printRow(n);
+        System.out.println();
     }
     public static void main(String[] args) {
         //Scanner sc = new Scanner(System.in);
         WriteTriangleRecursive writeTriangle = new WriteTriangleRecursive();
-        writeTriangle.printTriangle(4);
+        writeTriangle.printTriangle(5);
     }
 }
